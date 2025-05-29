8d55f146439ea840d673a983274ea73b1db9dee2
Gabriel Silveira <bsi.silveira@gmail.com>
2025-05-29 09:21:57 -0300

    feat: :sparkles: Move application run logic under the main guard

--- app/main.py
+++ app/main.py
@@ -16,3 +16,9 @@ def square(x: int):
 @app.get("/double/{x}")
 def double(x: int):
     return {"result": x * 2}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
